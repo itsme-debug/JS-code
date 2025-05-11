32222e60e88162edb16d47fafce9965505f7e404
itsme-debug <lavhalepiyush44@gmail.com>
2025-05-11 18:03:53 +0000

    comparision

---
+++ 01_basics/4_comparisions.js
@@ -0,0 +1,12 @@
+//  same datatype comparision
+console.log(2>2);
+console.log(2>=2);
+console.log(2<=2);
+console.log(2==2);
+
+
+// different datatype comparision
+console.log("2" == 2);
+
+// === operator compares datatype
+console.log("2" === 2);
