f428af08ddfde2a78ca8989c6591190ba764d15f
itsme-debug <lavhalepiyush44@gmail.com>
2025-05-11 07:35:58 +0000

    conversion

---
+++ 01_basics/3_conversion.js
@@ -0,0 +1,19 @@
+"use strict"
+let num = undefined
+let valu_num = Number(num)
+console.log(typeof valu_num)
+//console.log(num)
+//console.log(valu_num);
+/**
+ * 33 => 33
+ * acf33 => NAN
+ */
+let justice = "piyush"
+let boolean_justice = Boolean(justice)
+console.log(justice);
+console.log(boolean_justice);
+console.log(typeof boolean_justice);
+/**
+ * string,number,1 => true
+ * null,undeefined,0 => false
+ */
