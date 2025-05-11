92e1c9173343b18c01989253c58a740a872c51cd
itsme-debug <lavhalepiyush44@gmail.com>
2025-05-11 05:18:59 +0000

    datatypes

---
+++ 01_basics/1_basics.js
@@ -0,0 +1,7 @@
+const account_id ="123un"
+let account_name ="pikachu"
+var account_date="20-03-24"
+account_state= "maharashtra"
+let account_phoneno;
+
+console.table([account_id,account_name,account_date,account_state,account_phoneno])
---
+++ 01_basics/2_datatypes.js
@@ -0,0 +1,13 @@
+"use strict"
+console.log(typeof null)// object
+console.log(typeof undefined)// undefined
+
+// primitive datatypes 
+/*
+number => 2 to the power 53
+bigInt
+string => ""
+boolean => true/false 
+null => standalone value 
+undefined => 
+*/
